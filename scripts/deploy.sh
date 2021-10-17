41c7fbf0492f2f17acf2e6c36aab3a3178ebb465
Teemu Pääkkönen <paakkonen.teemu@gmail.com>
2021-10-17 18:08:41 +0300

    Make deploy script atomic

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-cd ~/modulate
-git fetch origin main
-git reset --hard origin/main
+set -xe
+
+rm -rf build
+git clone https://github.com/oamaok/modulate.git build
+
+cd build
 yarn
 yarn build
 yarn run-migrations
+
+cd ~
+rm -rf modulate.previous
+mv modulate modulate.previous
+mv build modulate
+
+cd modulate
 pm2 restart ecosystem.config.js
