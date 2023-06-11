fed69ecb51bb61fc0c8c37f0706ae5cac81f0252
Teemu Pääkkönen <paakkonen.teemu@gmail.com>
2023-06-12 01:17:31 +0300

    Debounce parameter updates

--- client/src/modules.ts
+++ client/src/modules.ts
@@ -4,6 +4,7 @@ import * as engine from './engine'
 import { Module } from '@modulate/worklets/src/modules'
 import { IndexOf } from '@modulate/common/types'
 
+
 export const connectKnobToParam = <
   M extends Module,
   N extends M['parameters'][number]
@@ -12,13 +13,28 @@ export const connectKnobToParam = <
   knob: string,
   index: IndexOf<M['parameters'], N>
 ) => {
+  let lastUpdate = 0
+  let timeout = -1 
+
   useEffect(() => {
     const knobs = getModuleKnobs(moduleId)
 
     if (knobs) {
       const knobValue = knobs[knob]
       if (typeof knobValue !== 'undefined') {
-        engine.setParameterValue(moduleId, index, knobValue)
+        // Debounce all parameter updates to only occurr at most every 30ms
+        const now = Date.now()
+        if (lastUpdate + 30 < now) {
+          lastUpdate = now
+          clearTimeout(timeout)
+          engine.setParameterValue(moduleId, index, knobValue)
+        } else {
+          clearTimeout(timeout)
+          timeout = setTimeout(() => {
+            lastUpdate = Date.now()
+            engine.setParameterValue(moduleId, index, knobValue)
+          }, 100) as unknown as number
+        }
       }
     }
   })
