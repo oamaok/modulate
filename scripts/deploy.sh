fa4e7bbc706158e3330e9d21160e87abe30c5728
Teemu Pääkkönen <paakkonen.teemu@gmail.com>
2023-06-18 23:46:26 +0300

    Add toolchain updates to deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,6 +6,10 @@ source $HOME/.cargo/env
 rm -rf build
 git clone https://github.com/oamaok/modulate.git build
 
+rustup toolchain install nightly-x86_64-unknown-linux-gnu
+rustup component add rust-src --toolchain nightly-x86_64-unknown-linux-gnu
+curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
+
 cd build
 yarn
 yarn build
