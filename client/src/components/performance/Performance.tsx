e01288e12b147b92d415968f1aa7d068bbfdf169
Teemu Pääkkönen <paakkonen.teemu@gmail.com>
2023-07-17 08:02:01 +0300

    Make performance display less prominent

--- client/src/components/performance/Performance.tsx
+++ client/src/components/performance/Performance.tsx
@@ -10,6 +10,8 @@ const Performance = () => {
       const canvas = canvasRef.current
 
       if (!canvas) return
+      const width = canvas.width
+      const height = canvas.height
       const context = canvas.getContext('2d')!
 
       const timers = engine.getWorkerTimers()
@@ -22,13 +24,12 @@ const Performance = () => {
       total /= timers.length
       total /= 128 / 44100
       total /= 1000
-      const imageData = context.getImageData(0, 0, 200, 100)
-      context.fillStyle = '#000'
-      context.fillRect(199, 0, 1, 100)
+      const imageData = context.getImageData(0, 0, width, height)
+      context.clearRect(width - 1, 0, 1, height)
       context.putImageData(imageData, -1, 0)
       context.fillStyle = `hsl(${120 - total * 120}deg 80% 50%)`
-      context.fillRect(199, (1 - total) * 100, 1, total * 100)
-    }, 100)
+      context.fillRect(width - 1, (1 - total) * height, 1, total * height)
+    }, 50)
 
     return () => clearInterval(interval)
   })
@@ -37,8 +38,8 @@ const Performance = () => {
     <canvas
       className={css('performance')}
       ref={canvasRef}
-      width="200px"
-      height="100px"
+      width="200"
+      height="100"
     />
   )
 }
